137129efe5cbcbfb4c1208cffb8114712e876349
cowwang <129141234+cowwang@users.noreply.github.com>
2023-03-28 13:37:31 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,9 @@ export default function Home() {
         <Header title="Welcome to my app!" />
    <div class="iframebox">
     <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRTpCiSsjQW1yjLvHZzfJ7RnUD_f6RArjMwLEdANWynM-pdjvWJtEMzx-3a_TfC4Obpd9i67z3ly4RB/pubhtml?gid=696319208&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-        <p className="description">
     </div>
+        <p className="description">
+    
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
